175edcedbfea0ea41a0e5108550499d1f28c8c6c
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-04-09 17:00:44 -0700

    Update Making predictions.py

--- Machine-Learning/Intro to Machine Learning/Making predictions.py
+++ Machine-Learning/Intro to Machine Learning/Making predictions.py
@@ -35,3 +35,10 @@ linearModel.predict(X)
 # distance = 2.0 miles, 1.0, 5.0
 Xnew = pd.DataFrame({'distance': [2.0, 1.0, 5.0]})
 print(linearModel.predict(Xnew))
+
+# The plot shows a weak relationship between actual price and predicted price
+p = sns.scatterplot(x=np.ravel(y), y=np.ravel(pred))
+p.set_xlabel('Actual price', fontsize=14)
+p.set_ylabel('Predicted price', fontsize=14)
+plt.xlim(0, 100)
+plt.ylim(0, 100)
