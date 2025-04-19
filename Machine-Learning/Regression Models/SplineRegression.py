a48a05540b0863e4ddb58b4d1df4a95f33a40634
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-04-18 17:38:28 -0700

    Update SplineRegression.py

--- Machine-Learning/Regression Models/SplineRegression.py
+++ Machine-Learning/Regression Models/SplineRegression.py
@@ -36,3 +36,22 @@ p.set_ylabel('Physical well-being index', fontsize=14)
 plt.show()
 
 LSRModel.predict(splinefit.transform([[-0.76]]))
+
+## Spline with 5 Knots
+# Transofrm input feature into matrix needed for the spline regression model
+spline = SplineTransformer(degree=1, n_knots=5, knots='quantile')
+splinefit = spline.fit(X)
+X_2 = spline.fit_transform(X)
+
+LSRModel = LinearRegression()
+LSRModel.fit(X_2,y)
+
+x_vals = np.linspace(min(wellbeing['NAI']), max(wellbeing['NAI']), 100)
+x_valsT = splinefit.transform(x_vals.reshape(-1,1))
+y_vals = LSRModel.predict(x_valsT)
+
+p = sns.regplot(data=wellbeing, x="NAI", y="WBI_Physical", ci=False, line_kws={'ls':'--', "color": "black"})
+plt.plot(x_vals, y_vals, color='#3ca02c', linewidth=3)
+p.set_xlabel('Natural amenities index', fontsize=14)
+p.set_ylabel('Physical well-being index', fontsize=14)
+plt.show()
